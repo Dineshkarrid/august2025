49dc4db3a7399a78641137b98c28914552ccfcb6
dinesh <dkdhini1@gamil.com>
2025-08-16 17:57:11 +0530

    update lab6

--- src/test/java/Pack1/Lab6_selenium.java
+++ src/test/java/Pack1/Lab6_selenium.java
@@ -1,10 +1,14 @@
 package Pack1;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -13,6 +17,7 @@ public class Lab6_selenium {
 	public static void main(String[] args) throws InterruptedException {
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver=new ChromeDriver();
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 		driver.get("https://tutorialsninja.com/demo/index.php?");
 		System.out.println(driver.getCurrentUrl());
 	    driver.findElement(By.linkText("Components")).click();
@@ -23,7 +28,21 @@ public class Lab6_selenium {
       driver.findElement(By.xpath("//*[@id=\"content\"]/div[3]/div[1]/div/div[2]/div[2]/button[1]")).click();
 	  Thread.sleep(3000); 
      driver.findElement(By.xpath("//*[@id=\"content\"]/div[1]/div[1]/ul[2]/li[2]/a")).click();
-		
+     WebElement specTable = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//table[@class='table table-bordered']")));
+     System.out.println("Specification present: " + specTable.isDisplayed());
+     driver.findElement(By.xpath("//*[@id=\"content\"]/div[1]/div[2]/div[1]/button[1]")).click();
+     WebElement wishlistMsg = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".alert-success")));
+     System.out.println("Wish list message: " + wishlistMsg.getText());
+     driver.findElement(By.xpath("//input[@name='search']")).sendKeys("Mobile");
+     driver.findElement(By.xpath("//*[@id=\"search\"]/span/button")).click();
+     driver.findElement(By.xpath("//*[@id=\"description\"]")).click();
+     driver.findElement(By.id("button-search")).click();
+     driver.findElement(By.linkText("HTC Touch HD")).click();
+     WebElement quant=driver.findElement(By.id("input-quantity"));
+     quant.clear();
+     quant.sendKeys("3");
+     driver.findElement(By.id("button-cart")).click();
+
 
 	}
 
