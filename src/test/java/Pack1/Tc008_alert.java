6b4c3718ddc9b054ead6de0e8466b94aa95beaae
dinesh <dkdhini1@gamil.com>
2025-08-16 10:54:44 +0530

    add selenium labs

---
+++ src/test/java/Pack1/Tc008_alert.java
@@ -0,0 +1,31 @@
+package Pack1;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Tc008_alert {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://letcode.in/alert\r\n");
+		driver.findElement(By.id("accept")).click();
+		Alert salert=driver.switchTo().alert();
+		System.out.println("message is  : "+salert.getText());
+		salert.accept();
+        driver.findElement(By.id("confirm")).click();
+Alert calert =driver.switchTo().alert() ;
+System.out.println("message is :  "+calert.getText());
+calert.dismiss();
+driver.findElement(By.id("prompt")).click();
+Alert palert=driver.switchTo().alert();
+System.out.println("message is : "+palert.getText());
+palert.sendKeys("dinesh");
+//palert.accept();
+}
+
+}
